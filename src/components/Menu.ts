3128911bf3a3f21bfc0082d8baf2073b11ea803d
Vitaliy Mykhailiuk AQA <mihaylukvv@gmail.com>
2026-03-08 17:46:20 +0200

    [IMDB DEMO] Quick fix menu overlaping case

--- src/components/Menu.ts
+++ src/components/Menu.ts
@@ -1,4 +1,4 @@
-import { Page, Locator } from '@playwright/test';
+import { Page, Locator, expect } from '@playwright/test';
 
 /**
  * Menu Component
@@ -9,19 +9,20 @@ import { Page, Locator } from '@playwright/test';
 export class Menu {
     protected readonly page: Page;
     readonly menuButton: Locator;
+    readonly menuPanel: Locator;
 
     constructor(page: Page) {
         this.page = page;
         
         this.menuButton = this.page.getByText('Menu', { exact: true });
+        this.menuPanel = this.page.getByTestId('panel').first();
     }
-
     /**
      * Returns a menu item locator by name.
      * @param name - The text or regex to find the menu item
      */
     menuItem(name: string | RegExp): Locator {
-        return this.page.getByText(name);
+        return this.menuPanel.getByText(name);
     }
 
     //=============================
@@ -31,20 +32,22 @@ export class Menu {
      * Verifies that the menu button is visible.
      */
     async verifyMenuVisible() {
-        await this.menuButton.waitFor({ state: 'visible' });
+        await expect(this.menuButton).toBeVisible();
     }
     /**
      * Opens the burger menu.
      */
     async openMenu() {
+        await this.page.waitForTimeout(3000); // TODO:wait for animations to finish. Only for demo
+
         await this.menuButton.click();
+        await expect(this.menuPanel).toBeVisible();
     }
     /**
      * Clicks a menu item by name.
      */
     async clickMenuItem(name: string | RegExp) {
         const item = this.menuItem(name);
-        await item.waitFor({ state: 'visible', timeout: 5000 });
         await item.click();
     }
     /**
--- test-data/Site.ts
+++ test-data/Site.ts
@@ -1,5 +1,5 @@
 export const Site = {
   title: "IMDb: Ratings, Reviews, and Where to Watch the Best Movies & TV Shows",
   url: "https://www.imdb.com/",
-  menuRatingItem: "Top 250 Movies"
+  menuRatingItem: "Top 250 movies"
 };
